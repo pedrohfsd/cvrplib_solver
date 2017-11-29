5d66a8f538cc4faaab7471d43168f6549679bb6c
Rafael Martinelli <martinelli@puc-rio.br>
2017-11-28 22:19:56 -0200

    . Silly changes.

--- cvrplib_solver/CapacityCut.h
+++ cvrplib_solver/CapacityCut.h
@@ -3,9 +3,15 @@
 
 #include "Cut.h"
 
-class CapacityCut : public Cut{
+class CapacityCut : public Cut
+{
 public:
 	CapacityCut();
+
+	int ks;
+	std::vector<int> vertices;
+
+	void build();
 };
 
 #endif //CAPACITYCUT_H
--- cvrplib_solver/Cut.h
+++ cvrplib_solver/Cut.h
@@ -2,20 +2,23 @@
 #define CUT_H
 
 #include <vector>
-#include "Vertex.h"
 
-class Cut {
+class Cut
+{
 public:
-	std::vector<Edge> edges;
-	std::vector<int> vertices;
-	int sense = 1;
-	int ks; // minimum number of cars needed
-	int rhs;
-	static const int greater_equal = 1;
-	static const int equal = 0;
-	static const int lower_equal = -1;
 	Cut();
 	Cut(int rhs, int sense);
+
+	enum Sense
+	{
+		greater_equal,
+		equal,
+		lower_equal,
+	};
+
+	std::vector<std::pair<int, double>> coeff;
+	Sense sense;
+	double rhs;
 };
 
 #endif //CUT_H
